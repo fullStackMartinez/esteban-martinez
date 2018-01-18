59eee01e69c906c2dd6a55886c7f45dbf2802c69
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-17 21:05:34 -0700

    -Set up my directory tree public_html>documentation>milestone-1.php -Set up my initial html mark up

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,8 +7,17 @@
 	<body>
 		<header><h1>Milestone 1</h1></header>
 		<main>
+			<h2>Purpose, Audience, Goal</h2>
 			<section>
-				<h3></h3>
+				<h3>Purpose</h3>
+				<p>As a new web developer and designer, it is very important that I establish myself as a professional in the development world. The purpose of building and maintaining a personal Web Portfolio is to demonstrate to potential clients the quality and range of my work. It is the visual representation of my work history, and also a chance to communicate quickly who I am as a developer.</p>
+			</section>
+			<section>
+				<h3>Audience</h3>
+				<p>What is an artist without someone to appreciate their work? I intend to build this portfolio for a range of professionals in this field, but mainly with potential clients in mind. My audience will include those who need work done, like-minded people with connections and networks in this industry and those who are looking for talent.</p>
+			</section>
+			<section>
+				<h3>Goal</h3>
 			</section>
 		</main>
 
