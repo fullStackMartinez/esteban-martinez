2a3c6ffe6ffd09019bb691a906157360fac49001
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-17 19:59:30 -0700

    -Set up my directory tree public_html>documentation>milestone-1.php -Set up my initial html mark up

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 1 | Milestone 1</title>
+	</head>
+	<body>
+		<header><h1>Milestone 1</h1></header>
+		<main>
+			<section>
+				<h3></h3>
+			</section>
+		</main>
+
+
+
+
+
+
+
+
+
+
+	</body>
+
+
+
+
+</html>
