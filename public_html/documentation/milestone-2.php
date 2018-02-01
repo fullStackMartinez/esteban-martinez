5b69de223166985b59632543127382b7892dba45
Esteban Martinez <fullstackmartinez@gmail.com>
2018-01-31 23:25:45 -0700

    added Content strategy to milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">i
+		<meta charset="UTF-8">
 		<title>Milestone 2a</title>
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
-		<p></p>
+		<p>My personal professional portfolio will be made to be mobile first, and scalable upwards in order to be seen on a desktop. I will have 4 sections, each with links in the nav bar:</p>
+		<h2>About Me - Landing page</h2>
+		<p>When someone, whether an employer or future client, visits my site, they will be greeted with a small, 1 sentence blurb about who I am and what kind of work they can expect from me</p>
+		<blockquote>&quot;Welcome, I'm Esteban Martinez, a New Mexico bred Full Stack Web Developer who enjoys building on the web, big or small. If you need a web presence, or if you are an employer looking to strengthen your team, take a peak at my portfolio and let's get to work!&quot;</blockquote>
+		<p>It will also have a profile picture with all my personal links underneath with their respective icons: GitHub, LinkedIn, Twitter, Facebook and email contact</p>
+		<h4>Other things to include briefly in my about me:</h4>
+		<ul>
+			<li>Stack that I use</li>
+			<li>Languages</li>
+			<li>Overview of my career and accolades</li>
+		</ul>
+		<h2>Works/Portfolio</h2>
+		<p>My pride and joy, the physical work and projects I have created or contributed on. this is a showcase of all my work and will be the first thing that is presented after my welcome message. I think it is important to capture the eye of the client/employer right away,
+			keep them on my page by immediately seeing what they really want to know: What does my work look like.</p>
+		<p>I will have a grid of my works done, 4 boxes with a quick thumbnail and the site name underneath. The visitor can hover over and a little box will pop up that has two clickable options, one to see the site and one to see the wireframe/ux design I worked on to show them that I know how to plan and desing, and they click it to go directly to the site
+			In this section, I will highlight through a table all my languages that I work in, maybe put little icons underneath the project thumbnail to show what tools/languages I used to build that specific site</p>
+		<h2>Feed</h2>
+		<p>An important aspect of myself as a professional in this field is to establish myself as an expert in what I work with. I also want to get my name out there into the world wide web so that I can be more easily found. I've decided to include a Feed page where I can link other contributions I've made to the web development world, specifically media works, such as YouTube tutorials or Medium blog posts.</p>
+		<h2>Contact</h2>
+		<p>I want my future client or employer to be able to contact me on any page of my personal website. The last page that I have included is a simple contact form, with a Captcha security feature. But I have also included an email icon on each pages' navigation bar, so that at anytime they can press that, and the same simple contact form will drop down for them to complete at a moments notice.</p>
 	</body>
 </html>
