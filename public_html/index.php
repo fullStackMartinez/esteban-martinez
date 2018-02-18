850289b1e7fd36cc335b3728bb4ac44e714c16e4
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-18 10:34:58 -0700

    added font awesome script in head took out jumbotron to try something new

--- public_html/index.php
+++ public_html/index.php
@@ -8,6 +8,7 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<title>Esteban Martinez | Web Developer</title>
 	</head>
 	<body>
@@ -15,9 +16,9 @@
 		<nav class="navbar navbar-expand-md navbar-light bg-faded">
 			<a class="navbar-brand" href="#">Esteban Martinez</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
+				<span class="navbar-toggler-icon"><i class="fas fa-code"></i></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarNav">
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 				<ul class="navbar-nav">
 					<li class="nav-item active">
 						<a class="nav-link" href="#">Who Is Esteban? <span class="sr-only">(current)</span></a>
@@ -31,29 +32,7 @@
 				</ul>
 			</div>
 		</nav>
-		<!--HOME SECTION JUMBOTRON-->
-		<header id="home">
-			<div class="filter">
-				<div class="middle">
-					<div class="container">
-						<div class="row">
-							<div class="col-lg-8">
-								<h3 class="display-8"> Welcome, I'm Esteban Martinez, a New Mexico bred <strong>Full Stack Web Developer</strong></h3>
-								<div class="d-flex flex-row">
-									<p class="p-4 align-self-start">Enjoys building on the web, big or small</p>
-									<p class="p-4 align-self-end">looking to strengthen your team</p>
+		<!--HOME SECTION WITH PICTURE AND A QUICK INTRO TEXT-->
 
-								</div>
-							</div>
-							<div class="col-lg-4">
-								<img class="img-fluid d-block" src="image/me.jpg" alt="me">
-
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		</header>
 	</body>
 </html>
