c2057e1ca88f3f89e1d9c26a1c624f5d20532282
Esteban Martinez <fullstackmartinez@gmail.com>
2018-02-19 20:14:50 -0700

    started contact form section
    added css/style.css to add some modifications

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,7 @@
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"
 				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
 				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
 				  crossorigin="anonymous"></script>
@@ -15,36 +16,34 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
 				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 				  crossorigin="anonymous"></script>
-
 		<title>Esteban Martinez</title>
 	</head>
 	<body>
 		<!--Nav bar-->
 		<div id="hero">
 			<div class="container">
+			<nav class="navbar navbar-expand-sm navbar-light bg-light pb-3">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span></button>
 
-					<nav class="navbar navbar-expand-sm navbar-light bg-faded">
-						<a class="navbar-brand" href="#">Esteban Martinez</a>
-						<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-								  data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-								  aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-							<ul class="navbar-nav">
-								<li class="nav-item">
-									<a class="nav-link" href="#">Who Is Esteban? </a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="#">Works</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="#">Contact</a>
-								</li>
-							</ul>
-						</div>
-					</nav>
-
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="nav navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About Me</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Portfolio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
 				<div class="row hero-content">
 					<div class="col-md-6 col-sm-12">
 						<h1 class="text-uppercase text-center text-md-left">Welcome, I'm Esteban Martinez</h1>
@@ -54,6 +53,7 @@
 				</div>
 			</div>
 		</div>
+
 
 
 		<!--HOME SECTION WITH QUICK INTRO TEXT/ USE CSS TO PUT BACKGROUND IMAGE FULL SCREEN-->
@@ -154,14 +154,6 @@
 			</div>
 
 		</section>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-				  crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-				  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-				  crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-				  crossorigin="anonymous"></script>
+
 	</body>
 </html>
