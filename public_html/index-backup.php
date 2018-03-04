11e694e893652e0857b6375276dc168c92748249
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-04 12:45:00 -0700

    cleaned up my Skills section, rebuilding landing page.
    created index-backup.php and style-backup.css with my old code, just in case I want to go back to something

--- public_html/index-backup.php
+++ public_html/index-backup.php
@@ -16,41 +16,46 @@
 				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 				  crossorigin="anonymous"></script>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<title>Esteban Martinez</title>
 	</head>
 	<body>
 		<!--Nav bar-->
-		<nav class="navbar navbar-expand-lg navbar fixed-top navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+		<section>
+			<div id="hero">
+				<div class="container">
+					<nav class="navbar navbar-expand-md navbar-light bg-light">
+						<a class="navbar-brand" href="#">EstebanM</a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+								  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span></button>
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Skills</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Portfolio</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Contact Me</a>
-					</li>
-				</ul>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="nav navbar-nav ml-auto">
+								<li class="nav-item active">
+									<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">About Me</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Portfolio</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Contact</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+
+					<div class="row hero-content justify-content-end pt-lg-5 pb-lg-5 pt-2">
+						<div class="col-md-6 col-sm-6 col-xs-6">
+							<h1 class="text-uppercase text-center text-lg-right">Welcome, I'm Esteban Martinez</h1>
+							<h4 class="text-uppercase text-center text-lg-right">Full Stack Web Developer</h4>
+						</div>
+					</div>
+				</div>
 			</div>
-		</nav>
-		<!--HERO SECTION-->
-		<div id="hero">
-			<div class="landing-text">
-				<h1>Welcome, I'm Esteban</h1>
-				<h3>Junior Full Stack Developer</h3>
-			</div>
-		</div>
+		</section>
 		<!--About Me-->
 		<section>
 			<div class="about-me">
@@ -85,36 +90,58 @@
 							<i class="fas fa-futbol fa-2x mb-2"></i>
 						</div>
 						<h4 class="text-center ">Team Player</h4>
-						<p class="text-center">I thrive in a team setting. My history with competitive team sports has taught me how to work with different personalities, teams and game plans. I know how to push myself, and have the belief that winning teams begin with strong individuals. </p>
+						<p class="text-center">Here will be the team player stuff</p>
 					</div>
 					<div class="col-sm-12 col-md-4">
 						<div class="d-flex justify-content-center">
-							<i class="far fa-lightbulb fa-2x mb-2 pr-2"></i>
-							<i class="far fa-comments fa-2x mb-2 pl-2"></i>
+							<i class="fas fa-futbol fa-2x mb-2"></i>
 						</div>
-						<h4 class="text-center">UX/UI Ideas</h4>
-						<p class="text-center">I collaborate with clients and employers to brainstorm ideas into well thought out designs. Being able to clearly communicate and quickly adapt is something I pride myself in.</p>
+						<h4 class="text-center ">Team Player</h4>
+						<p class="text-center">Here will be the team player stuff</p>
 					</div>
 					<div class="col-sm-12 col-md-4">
 						<div class="d-flex justify-content-center">
-							<i class="fas fa-code fa-2x mb-2 pr-2"></i>
-							<i class="fas fa-database fa-2x mb-2 pl-2"></i>
+							<i class="fas fa-futbol fa-2x mb-2"></i>
 						</div>
-						<h4 class="text-center">Coding Skills</h4>
-						<dl class="text-center">
-							<dt>Front-End</dt>
-							<dd>HTML 5 | CSS3 <br> JavaScript | Bootstrap 4</dd>
-							<dt>Back-End</dt>
-							<dd>PHP</dd>
-							<dd>MySQL</dd>
-
-						</dl>
+						<h4 class="text-center ">Team Player</h4>
+						<p class="text-center">Here will be the team player stuff</p>
 					</div>
 				</div>
 			</div>
 		</div>
 		<!--PORTFOLIO/WORKS-->
+		<section>
+			<div class="container">
+				<div class="card-deck">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">Main web site I want to show</h4>
+							<p class="card-text">Lorem ipsum dolor amet williamsburg tousled bitters, raclette kickstarter food
+								truck tumblr vexillologist seitan PBR&B.</p>
+						</div>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">second website I want to show</h4>
+							<p class="card-text">Tumblr butcher street art seitan heirloom cred bespoke roof party XOXO.
+								Semiotics
+								twee health goth selfies brunch bushwick.</p>
+						</div>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h4 class="card-title">Third website I want to show</h4>
+							<p class="card-text">Shabby chic hexagon jianbing scenester prism la croix direct trade pok pok
+								microdosing chillwave glossier neutra.</p>
 
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 		<!--CONTACT FORM-->
 		<div id="join-container" class="pt-lg-6 pb-lg-6 pt-5">
 			<div class="container">
