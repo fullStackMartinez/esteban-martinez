a845486a5bb58b0c1e36fc8b08499733a99adee0
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-05 20:25:47 -0700

    fixed padding in body

--- public_html/index.php
+++ public_html/index.php
@@ -17,11 +17,21 @@
 				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
 				  crossorigin="anonymous"></script>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<title>Esteban Martinez</title>
 	</head>
 	<body>
 		<!--Nav bar-->
-		<nav class="navbar navbar-expand-md navbar fixed-top navbar-dark bg-dark">
+		<nav class="navbar navbar-expand-md navbar fixed-top navbar-trans navbar-dark bg-dark">
 			<a class="navbar-brand pl-2" href="#">Esteban Martinez</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 					  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,13 +44,13 @@
 						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Skills</a>
+						<a class="nav-link" href="#skill">Skills</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Portfolio</a>
+						<a class="nav-link" href="#portfolio">Portfolio</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Contact Me</a>
+						<a class="nav-link" href="#contact">Contact Me</a>
 					</li>
 				</ul>
 			</div>
@@ -50,8 +60,14 @@
 		<div id="hero">
 			<div class="container-fluid">
 				<div class="landing-text">
-						<h1 class="d-flex justify-content-end">Welcome!<br> I'm Esteban</h1>
+						<h1 class="d-flex justify-content-end">  Welcome!<br> I'm Esteban</h1>
 					<h3 class="d-flex justify-content-end">Junior Full Stack<br>Web Developer</h3>
+					<ul class="d-flex justify-content-end">
+						<li><i class="fab fa-github-square fa-2x"></i></li>
+						<li><i class="fab fa-linkedin fa-2x"></i></li>
+						<li><i class="fab fa-facebook fa-2x"></i></li>
+						<li><i class="fas fa-envelope fa-2x"></i></li>
+					</ul>
 				</div>
 			</div>
 		</div>
@@ -71,7 +87,7 @@
 							<!--IMAGE IS PLACEHOLDER FOR NOW, ANDREW MAKING ME ART PIECE-->
 							<img src="image/download.jpg" alt="picture of me placeholder" class="img-fluid d-block mx-auto">
 						</div>
-						<div class="col-lg-6 col-md-6 col-sm-12 item-text">
+						<div class="col-lg-6 col-md-6 col-sm-12 item-text pt-4">
 							<p>Welcome, I'm Esteban Martinez, a New Mexico bred <strong>Full Stack Web Developer</strong>
 								who
 								enjoys building on the web, big or small. If you need a web <strong>presence</strong>, or if
@@ -85,20 +101,25 @@
 			</div>
 		</section>
 		<!--HOME SECTION WITH THE 3 COLUMNS OF MY EXPERTISE-->
-		<section>
-			<div class="home pt-5 pb-5">
+		<section id="skill">
+			<div class="home pt-5">
 				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 d-flex justify-content-center pb-4">
+							<h1>Skills</h1>
+						</div>
+					</div>
 					<div class="row">
 						<div class="col-sm-12 col-md-4">
 							<div class="d-flex justify-content-center">
-								<i class="fas fa-futbol fa-2x mb-2"></i>
+								<i class="fas fa-futbol fa-2x mb-2 pb-2"></i>
 							</div>
 							<h4 class="text-center ">Team Player</h4>
 							<p class="text-center">My history with competitive team sports has
 								taught me how to work with different personalities, teams and game plans. I know how to push
 								myself, and have the belief that winning teams begin with strong individuals. </p>
 						</div>
-						<div class="col-sm-12 col-md-4 pt-3">
+						<div class="col-sm-12 col-md-4 pb-3">
 							<div class="d-flex justify-content-center">
 								<i class="fas fa-lightbulb fa-2x mb-2 pr-2"></i>
 								<i class="fas fa-comments fa-2x mb-2 pl-2"></i>
@@ -108,7 +129,7 @@
 								thought out designs. Being able to clearly communicate and quickly adapt is something I pride
 								myself in.</p>
 						</div>
-						<div class="col-sm-12 col-md-4 pt-3">
+						<div class="col-sm-12 col-md-4">
 							<div class="d-flex justify-content-center">
 								<i class="fas fa-code fa-2x mb-2 pr-2"></i>
 								<i class="fas fa-database fa-2x mb-2 pl-2"></i>
@@ -127,7 +148,7 @@
 			</div>
 		</section>
 		<!--PORTFOLIO/WORKS-->
-		<section>
+		<section id="portfolio" class="pt-3">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12 d-flex justify-content-center pb-4">
@@ -155,36 +176,44 @@
 			</div>
 		</section>
 		<!--CONTACT FORM-->
-		<section>
+		<section id="contact">
 			<div id="join-container" class="pt-lg-6 pb-lg-6 pt-5">
 				<div class="container">
 					<h2 class="text-uppercase text-center">Interested in working together?</h2>
 					<h4 class="text-uppercase text-center">Contact Me</h4>
-					<form>
+					<form id="contact-form" action="php/mailer.php" method="post">
 						<div class="form-group row pt-lg-5 pb-lg-5 pt-5">
 							<div class="col-sm-12 col-md-6 offset-md-3">
-								<label for="formInput">Name</label>
-								<input type="text" class="form-control mb-4" id="formInput" aria-describedby="emailHelp"
+								<label for="contactName">Name</label>
+								<input type="text" class="form-control mb-4" id="contactName" name="contactName" aria-describedby="emailHelp"
 										 placeholder="Name">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
-								<label for="formInput2">Email Address</label>
-								<input type="email" class="form-control mb-4" id="formInput2" aria-describedby="emailHelp"
+								<label for="contactEmail">Email Address</label>
+								<input type="email" class="form-control mb-4" id="contactEmail" name="contactEmail" aria-describedby="emailHelp"
 										 placeholder="Enter email address">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
-								<label for="formInput3">Subject</label>
-								<input type="text" class="form-control mb-4" id="formInput3" aria-describedby="emailHelp"
+								<label for="contactSubject">Subject</label>
+								<input type="text" class="form-control mb-4" id="contactSubject" name="contactSubject" aria-describedby="emailHelp"
 										 placeholder="Email subject">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
-								<label for="textArea">A quick message</label>
-								<textarea class="form-control" id="textArea" rows="4"
+								<label for="contactMessage">A quick message</label>
+								<textarea class="form-control" id="contactMessage" name="contactMessage" rows="4"
 											 placeholder="What are you interested in?"></textarea>
 							</div>
 						</div>
+						<div class="g-recaptcha" data-sitekey="6Ld-yEoUAAAAAPStd7wJSm6jzHc-jnqBSSTV9fid"></div>
+
+
+						<button type="button" class="btn btn-primary btn-lg btn-block mb-3">Submit</button>
 					</form>
-					<button type="button" class="btn btn-primary btn-lg btn-block mb-3">Submit</button>
+					<div class="row">
+						<div class="col-xs-12">
+							<div id="output-area"></div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
