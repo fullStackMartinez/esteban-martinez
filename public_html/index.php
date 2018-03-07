3eca816d2dd2855aa3874186765cd7a02f661dca
Esteban Martinez <fullstackmartinez@gmail.com>
2018-03-06 23:18:41 -0700

    changed background image,
    made jumbotron

--- public_html/index.php
+++ public_html/index.php
@@ -18,9 +18,12 @@
 				  crossorigin="anonymous"></script>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -60,37 +63,48 @@
 		<div id="hero">
 			<div class="container-fluid">
 				<div class="landing-text">
-						<h1 class="d-flex justify-content-end">  Welcome!<br> I'm Esteban</h1>
-					<h3 class="d-flex justify-content-end">Junior Full Stack<br>Web Developer</h3>
+					<h1 class="display-4 d-flex justify-content-end"> Welcome!<br>I'm Esteban</h1>
+					<h3 class="display-6 d-flex justify-content-end">Junior Full Stack<br>Web Developer</h3>
 					<ul class="d-flex justify-content-end">
-						<li><a href="https://github.com/fullStackMartinez" target="_blank"><i class="fab fa-github-square fa-2x"></i></a></li>
-						<li><a href="https://www.linkedin.com/in/esteban-martinezABQ/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></li>
-						<li><i class="fab fa-facebook fa-2x"></i></li>
-						<li><a href="mailto:fullstackmartinez@gmail.com"> <i class="fas fa-envelope fa-2x"></i></a></li>
+						<li><a href="https://github.com/fullStackMartinez" target="_blank"><i
+									class="fab fa-github-square fa-2x" style="color: #1C242C"></i></a></li>
+						<li><a href="https://www.linkedin.com/in/esteban-martinezABQ/" target="_blank"><i
+									class="fab fa-linkedin fa-2x" style="color: #1C242C;"></i></a></li>
+						<li><i class="fab fa-facebook fa-2x" style="color: #1C242C"></i></li>
+						<li><a href="mailto:fullstackmartinez@gmail.com"> <i class="fas fa-envelope fa-2x" style="color: #1C242C"></i></a></li>
 					</ul>
 				</div>
 			</div>
 		</div>
-
-		<!--About Me-->
+		<div class="jumbotron">
+			<div class="container">
+				<h1 class="display-4">Fluid jumbotron</h1>
+				<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+			</div>
+		</div>
+		<!--About Me
 		<section>
 			<div class="about-me py-5">
 				<div class="container">
 					<div class="row justify-content-center">
 						<div class="col-lg-6 col-md-6 col-sm-12">
-							<!--IMAGE IS PLACEHOLDER FOR NOW, ANDREW MAKING ME ART PIECE-->
-							<img src="image/download.jpg" alt="picture of me placeholder" class="img-fluid d-block mx-auto mb-2">
+							<!--IMAGE IS PLACEHOLDER FOR NOW, ANDREW MAKING ME ART PIECE
+							<img src="image/download.jpg" alt="picture of me placeholder"
+								  class="img-fluid d-block mx-auto mb-2">
 						</div>
 						<div class="col-lg-6 col-md-6 col-sm-12 item-text pt-4">
 							<h3>
-								Fancy display heading
-								<small class="text-muted">With faded secondary text</small>
+								Esteban Martinez <br>
+								<small class="text-muted">I love to create. Whether the project is big or small I strive to do
+									my part as a team member. I am a Junior Level Developer looking for opportunities to sharpen
+									my skills.
+								</small>
 							</h3>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section>
+		</section>-->
 		<!--HOME SECTION WITH THE 3 COLUMNS OF MY EXPERTISE-->
 		<section id="skill">
 			<div class="home pt-5">
@@ -103,37 +117,36 @@
 					<div class="row">
 						<div class="col-sm-12 col-md-4">
 							<div class="d-flex justify-content-center">
-								<i class="fas fa-futbol fa-2x mb-2 pb-2"></i>
+								<i class="fas fa-futbol fa-3x mb-2 pb-2" style="color: #0080FF"></i>
 							</div>
 							<h4 class="text-center ">Team Player</h4>
 							<p class="text-center">My history with competitive team sports has
 								taught me how to work with different personalities, teams and game plans. I know how to push
 								myself, and have the belief that winning teams begin with strong individuals. </p>
 						</div>
-						<div class="col-sm-12 col-md-4 pb-3">
-							<div class="d-flex justify-content-center">
-								<i class="fas fa-lightbulb fa-2x mb-2 pr-2"></i>
-								<i class="fas fa-comments fa-2x mb-2 pl-2"></i>
-							</div>
-							<h4 class="text-center">UX/UI Ideas</h4>
-							<p class="text-center">I collaborate with clients and employers to brainstorm ideas into well
-								thought out designs. Being able to clearly communicate and quickly adapt is something I pride
-								myself in.</p>
+					<div class="col-sm-12 col-md-4 pb-3">
+						<div class="d-flex justify-content-center">
+							<i class="fas fa-lightbulb fa-3x mb-2 pr-2" style="color: #0080FF"></i>
+							<i class="fas fa-comments fa-3x mb-2 pl-2" style="color: #0080FF"></i>
 						</div>
-						<div class="col-sm-12 col-md-4">
-							<div class="d-flex justify-content-center">
-								<i class="fas fa-code fa-2x mb-2 pr-2"></i>
-								<i class="fas fa-database fa-2x mb-2 pl-2"></i>
-							</div>
-							<h4 class="text-center">Coding Skills</h4>
-							<dl class="text-center">
-								<dt>Front-End</dt>
-								<dd>HTML 5 | CSS3 <br> JavaScript | Bootstrap 4</dd>
-								<dt>Back-End</dt>
-								<dd>PHP</dd>
-								<dd>MySQL</dd>
-							</dl>
+						<h4 class="text-center">UX/UI Ideas</h4>
+						<p class="text-center">I collaborate with clients and employers to brainstorm ideas into well
+							thought out designs. Being able to clearly communicate and quickly adapt is something I pride
+							myself in.</p>
+					</div>
+					<div class="col-sm-12 col-md-4">
+						<div class="d-flex justify-content-center">
+							<i class="fas fa-code fa-3x mb-2 pr-2" style="color: #0080FF"></i>
+							<i class="fas fa-database fa-3x mb-2 pl-2" style="color: #0080FF"></i>
 						</div>
+						<h4 class="text-center">Coding Skills</h4>
+						<dl class="text-center">
+							<dt>Front-End</dt>
+							<dd>HTML 5 | CSS3 <br> JavaScript | Bootstrap 4</dd>
+							<dt>Back-End</dt>
+							<dd>PHP</dd>
+							<dd>MySQL</dd>
+						</dl>
 					</div>
 				</div>
 			</div>
@@ -176,17 +189,20 @@
 						<div class="form-group row pt-lg-5 pb-lg-5 pt-5">
 							<div class="col-sm-12 col-md-6 offset-md-3">
 								<label for="contactName">Name</label>
-								<input type="text" class="form-control mb-4" id="contactName" name="contactName" aria-describedby="emailHelp"
+								<input type="text" class="form-control mb-4" id="contactName" name="contactName"
+										 aria-describedby="emailHelp"
 										 placeholder="Name">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
 								<label for="contactEmail">Email Address</label>
-								<input type="email" class="form-control mb-4" id="contactEmail" name="contactEmail" aria-describedby="emailHelp"
+								<input type="email" class="form-control mb-4" id="contactEmail" name="contactEmail"
+										 aria-describedby="emailHelp"
 										 placeholder="Enter email address">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
 								<label for="contactSubject">Subject</label>
-								<input type="text" class="form-control mb-4" id="contactSubject" name="contactSubject" aria-describedby="emailHelp"
+								<input type="text" class="form-control mb-4" id="contactSubject" name="contactSubject"
+										 aria-describedby="emailHelp"
 										 placeholder="Email subject">
 							</div>
 							<div class="col-sm-12 col-md-6 offset-md-3">
@@ -199,7 +215,7 @@
 							<div class="g-recaptcha" data-sitekey="6Ld-yEoUAAAAAPStd7wJSm6jzHc-jnqBSSTV9fid"></div>
 						</div>
 						<div class="text-center">
-						<button class="btn btn-primary btn-lg mt-2 mb-3 text-center" type="submit">Submit</button>
+							<button class="btn btn-primary btn-lg mt-2 mb-3 text-center" type="submit">Submit</button>
 						</div>
 					</form>
 					<div class="row">
@@ -221,8 +237,10 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<ul class="d-flex display-inline justify-content-center">
-							<li><a href="https://github.com/fullStackMartinez" target="_blank"><i class="fab fa-github-square fa-2x"></i></a></li>
-							<li><a href="https://www.linkedin.com/in/esteban-martinezABQ/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></li>
+							<li><a href="https://github.com/fullStackMartinez" target="_blank"><i
+										class="fab fa-github-square fa-2x"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/esteban-martinezABQ/" target="_blank"><i
+										class="fab fa-linkedin fa-2x"></i></a></li>
 							<li><i class="fab fa-facebook fa-2x"></i></li>
 							<li><a href="mailto:fullstackmartinez@gmail.com"> <i class="fas fa-envelope fa-2x"></i></a></li>
 						</ul>
